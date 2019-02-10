383e919818325847d5b8b94b0216bcf3a802a8ce
Guilherme <guigehling@gmail.com>
2019-02-10 15:43:13 -0200

    Final do capítulo 8

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -5,5 +5,10 @@ module.exports = function (app) {
     app.get('/auth/github', passport.authenticate('github'));
     app.get('/auth/github/callback',
         passport.authenticate('github', { successRedirect: '/' }));
+    app.get('/logout',
+        function (req, res) {
+            req.logout(); //Função do passport
+            res.redirect('/');
+        });
 
 }
