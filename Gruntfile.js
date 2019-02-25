f27a48830e1495587acddfa0a8f20fbb882a8b7a
Guilherme <guigehling@gmail.com>
2019-02-25 20:01:42 -0300

    Recomeçar no capítulo 10.9

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        copy: {
+            project: {
+                expand: true,
+                cwd: '.',
+                src: ['**', '!Gruntfile.js', '!package.json', '!bower.json'],
+                dest: 'dist'
+            }
+        },
+        clean: {
+            dist: {
+                src: 'dist'
+            }
+        }
+
+    });
+
+    grunt.registerTask('dist', ['clean', 'copy']);
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
+
+};
