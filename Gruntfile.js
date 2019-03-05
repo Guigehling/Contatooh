fed54c0a19da16352bbec3fa46cbc44696a15231
Guilherme <guigehling@gmail.com>
2019-03-04 21:03:06 -0300

    Finalizado o capítulo 10

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,20 +9,44 @@ module.exports = function (grunt) {
                 dest: 'dist'
             }
         },
+
         clean: {
             dist: {
-                src: 'dist'
+                src: ['dist', '.tmp']
+            }
+        },
+
+        usemin: {
+            html: 'dist/app/views/**/*.ejs'
+        },
+
+        useminPrepare: {
+            options: {
+                root: 'dist/public',
+                dest: 'dist/public'
+            },
+            html: 'dist/app/views/**/*.ejs'
+        },
+
+        ngAnnotate: {
+            scripts: {
+                expand: true,
+                src: ['dist/public/js/**/*.js']
             }
         }
 
     });
 
+    grunt.registerTask('default', ['dist', 'minifica']);
     grunt.registerTask('dist', ['clean', 'copy']);
+    grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
+
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-ng-annotate');
     grunt.loadNpmTasks('grunt-usemin');
 
 };
